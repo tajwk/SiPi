7804df9a4c5567a07e0048a098967e9bc92b8029
tajwk <twurlkoth@tms-usa.com>
2025-06-24 16:21:39 -0700

    Update SiPi.py
    increment version for update test

--- SiPi.py
+++ SiPi.py
@@ -14,7 +14,7 @@ from flask import (
 )
 
 # Initial SiPi version (bump patch for simple fixes)
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 # Base directory for Git operations
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
